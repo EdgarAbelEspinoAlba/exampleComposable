0a1513179063526d570291e0d51d18f6dc200f71
Edgar Abel Espino Alba <abel_espino05@hotmail.com>
2022-12-20 13:33:32 -0600

    [UPDATE] RememberSeveable

--- app/src/main/java/com/example/mynewcompose/MainActivity.kt
+++ app/src/main/java/com/example/mynewcompose/MainActivity.kt
@@ -11,10 +11,12 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -43,7 +45,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyStateExample() {
 
-    var counter = remember { mutableStateOf(0) }
+    var counter by rememberSaveable { mutableStateOf(0) }
 
     Column(
         modifier = Modifier
@@ -53,12 +55,12 @@ fun MyStateExample() {
     ) {
         Button(
             onClick = {
-                counter.value += 1
+                counter += 1
             }
         ) {
             Text(text = "Pulsar")
         }
-        Text(text = "He sido pulsado ${counter.value} veces")
+        Text(text = "He sido pulsado $counter veces")
     }
 }
 
